abfe09aebcb582494c73f2adaf496937ce4fc9b2
Alfie_N <olisafredo@gmail.com>
2023-06-19 21:50:52 +0100

    001

---
+++ main.py
@@ -0,0 +1,17 @@
+#def greet():
+    #print(" T baby rocks")
+    #print(" tbaby alife")
+
+#greet()
+
+#funtions that allows for input
+
+def greet_with_name(name):
+    print(f"yes {name}")
+    print(f" T baby rocks, yes {name}")
+    print(f" tbaby {name}")
+
+greet_with_name("ALFRED")
+  
+
+  
